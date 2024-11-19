278c957b05471e31682efa6a449bd5a541f884b4
Odin Hoff Gardaa <odinhg@gmail.com>
2024-11-20 00:47:41 +0100

    works great, code need some cleaning up

--- mcts.py
+++ mcts.py
@@ -11,12 +11,8 @@ import random
 from time import time
 from board import read_board_from_file, playout, step, is_terminal, get_blobs, print_board
 
-# import deepcopy from copy module
-from copy import deepcopy
-
-
 class Node:
-    def __init__(self, board, parent=None, move=None):
+    def __init__(self, board, depth, parent=None, move=None):
         self.board = [row[:] for row in board] # Copy of the board state
         self.parent = parent # Parent node
         self.move = move # (x, y) coordinates of the move that led to this node
@@ -25,41 +21,54 @@ class Node:
         self.n_visits = 0 # Number of times this node has been visited
         self.average_score = 0.0 # Average score of the node
         self.square_sum_score = 0.0 # Sum of the square of the scores
+        self.depth = depth
 
     def is_leaf(self):
         return len(self.children) < len(self.blobs)
 
 class MCTS:
-    def __init__(self, board, selection_threshold=100):
-        self.root = Node(board)
+    """
+    Only use UTC for selection strategy if all children are explored
+    """
+    def __init__(self, board, selection_threshold=1000):
+        self.board = board
+        self.root = Node(board, depth=0)
         self.selection_threshold = selection_threshold
-        self.found_solution = False
+        self.solution_found = False
+        self.best_solution = None
 
-    def search(self, time_limit=30.0):
+    def search(self, time_limit=1.0):
         t0 = time()
-        t1 = time()
-        while time() - t0 < time_limit or not self.found_solution:
+        node = self.root
+        iter_count = 0
+        while time() - t0 < time_limit or not self.solution_found:
             node = self.select()
             if node.is_leaf():
                 node = self.expand(node)
             score = self.simulate(node)
             self.backpropagate(node, score)
+            iter_count += 1
+            print("Number of iterations:", iter_count)
 
-            if time() - t1 > 0.5 and self.found_solution:
-                print("Best solution so far:")
-                self.print_solution(print_boards=False)
-                t1 = time()
+        # print best solution by backtracking from the best leaf node
+        self.print_solution()
 
     def select(self):
         node = self.root
         while not node.is_leaf():
             if is_terminal(node.board):
-                self.found_solution = True
+                self.solution_found = True
+                if self.best_solution is None or node.depth < self.best_solution.depth:
+                    self.best_solution = node
                 return node
             node = self.selection_strategy(node)
         return node
 
     def selection_strategy(self, node):
+        if node.is_leaf():
+            node = self.expand(node)
+            return node
+
         if node.n_visits < self.selection_threshold:
             # pick a random child node 
             return random.choice(node.children)
@@ -68,8 +77,7 @@ class MCTS:
         max_uct_child = node.children[max_uct_index]
         return max_uct_child
 
-    #def uct(self, parent, child, C=0.5, D=1000):
-    def uct(self, parent, child, C=0.01, D=10000):
+    def uct(self, parent, child, C=0.1, D=100):
         # Modified UCT formula from equation (1) in [1]
         x_bar = child.average_score
         n = parent.n_visits
@@ -81,20 +89,24 @@ class MCTS:
     def simulate(self, node):
         # Random playout from the current node
         board_copy = [row[:] for row in node.board]
-        n_moves = playout(board_copy)
+        n_moves = playout(board_copy) + node.depth
         score = self.score(n_moves)
         return score
 
     def score(self, n_moves):
         # Scoring function for the playout
-        score = 1000 * math.exp(-n_moves / 20) 
+        #score = 5000 * math.exp(-(n_moves**2) / 1000)
+        #score = 10000 / (n_moves)
+        # 1 to 63 maps to 0 to 1
+        score = 1 - (n_moves - 1) / 63
+        score *= 10000
         return score
     
     def expand(self, node):
         blob = node.blobs[len(node.children)]
         board_copy = [row[:] for row in node.board]
         board_copy = step(board_copy, *blob)
-        child = Node(board_copy, parent=node, move=blob)
+        child = Node(board_copy, depth=node.depth + 1, parent=node, move=blob)
         node.children.append(child)
         return child
 
@@ -117,23 +129,24 @@ class MCTS:
         return 1 + max(self.max_depth(child) for child in node.children)
 
     def print_solution(self, print_boards=True):
-        # Print the best path and the board state after each move
+        if self.best_solution is None:
+            raise ValueError("No solution found")
+
+        node = self.best_solution
         moves = []
-        node = self.root
-        while node.children:
-            if print_boards:
-                print_board(node.board)
-            node = max(node.children, key=lambda child: child.average_score)
+        while node.parent:
             moves.append(node.move)
-            if print_boards:
-                print(node.move)
-                print()
-        print("Moves:", moves)
+            node = node.parent
+        moves.reverse()
+        print("Best solution:", moves)
         print("Number of moves:", len(moves))
 
-
 if __name__ == "__main__":
+    """
+    TODO:
+    - Remove unused code in board.py and mcts.py
+    - Run MCTS on the board with different random seeds and pick the best solution (multiprocessing) to prevent getting stuck in a local maximum 
+    """
     board = read_board_from_file("board.txt")
     mcts = MCTS(board)
-    mcts.search()
-    mcts.print_solution()
+    mcts.search(time_limit=10.0)
